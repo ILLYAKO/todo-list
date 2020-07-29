f957997fada8294a51675d559c6f1ea3c291f355
ilhaifa@yandex.ru <ilhaifa@yandex.ru>
2020-07-29 18:58:21 -0500

    Commit July 29,2020

--- src/App.js
+++ src/App.js
@@ -1,27 +1,60 @@
-import React from 'react';
-import TodoInput from './components/Todoinput'
-import TodoList from './components/TodoList'
+import React, { Component } from "react";
+import TodoInput from "./components/Todoinput";
+import TodoList from "./components/TodoList";
 
-import 'bootstrap/dist/css/bootstrap.min.css'
-import uuid from 'uuid'
+import "bootstrap/dist/css/bootstrap.min.css";
+// import uuid from "uuid/v1";
+import { v1 as uuid } from "uuid";
 
-function App() {
-  return (
-    <div className='container'>
-      <div className='row'>
-        <div className='col-10 mx-auto col-md-8 mt-4'>
-          <h3 className='text-capitalize text-center'>Todo Input</h3>
-        <TodoInput/>
-        <TodoList/>
+class App extends Component {
+  state = {
+    items: [],
+    id: uuid(),
+    item: "",
+    editItem: false,
+  };
+  handleChange = (e) => {
+    this.setState({
+      item: e.target.vaue,
+    });
+  };
 
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    const newItem = {
+      id: this.state.id,
+      title: this.state.item,
+    };
+    console.log(newItem);
+
+    const updatedItems = [...this.state.items, newItem];
+
+    this.setState({
+      items: updatedItems,
+      item: "",
+      id: uuid(),
+      editItem: false,
+    });
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-10 mx-auto col-md-8 mt-4">
+            <h3 className="text-capitalize text-center">Todo Input</h3>
+            <TodoInput
+              item={this.state.item}
+              handleChange={this.handleChange}
+              handleSubmit={this.handleSubmit}
+            />
+            <TodoList items={this.state.items} />
+          </div>
         </div>
       </div>
-
-    </div>
-
-
-
-  );
+    );
+  }
 }
 
 export default App;
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,17 +2,19 @@ import React, { Component } from "react";
 
 export default class TodoItem extends Component {
   render() {
+    const{title}=this.props
     return (
       <div>
         <li className="list-group-item text-capitalize d-flex justify-contebnt-between my-2">
-          <h6>title</h6>
+          <h6>{title}</h6>
           <div className="todi-icon">
             <span className="mx-2 text-success">
-              <i className="fas fa-pen">1</i>
+              <i className="fas fa-pen"></i>
             </span>
             <span className="mx-2 text-danger">
-              <i className="fas fa-trash">2</i>
+              <i className="fas fa-trash"></i>
             </span>
+            <item />
           </div>
         </li>
       </div>
